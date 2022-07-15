46ffab191f7d4bfa1fc0048e86173114987aa65a
namjiyun <namjiyun3275@gmail.com>
2022-07-15 21:02:33 +0900

    [FEAT] 사진 상세조회 service 구현

--- src/routes/photoRouter.ts
+++ src/routes/photoRouter.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { photoController } from '../controllers';
 import upload from '../config/multer';
 import PhotoController from '../controllers/PhotoController';
 // import auth from '../modules/auth';
@@ -8,4 +7,5 @@ import PhotoController from '../controllers/PhotoController';
 const router: Router = Router();
 
 router.post('/', upload.single('file'), PhotoController.createPhotoTag);
+router.get('/:photoId', PhotoController.getPhoto);
 export default router;
--- src/services/PhotoService.ts
+++ src/services/PhotoService.ts
@@ -60,7 +60,35 @@ const getTagByPhotoId = async (client: any, photoId: number, userId: number) => 
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
+const getPhotoById = async (client: any, photoId: number, userId: number) => {
+  const { rows } = await client.query(
+    `
+    SELECT id, image_url
+    FROM photo
+    WHERE id = $1
+    `,
+    [photoId],
+  );
+
+  const { rows: tag } = await client.query(
+    `
+    SELECT tag.id, tag.name
+    FROM photo, photo_tag, tag
+    WHERE photo.id = $1 AND photo.user_id = $2 AND photo.is_deleted = false
+    AND photo.id = photo_tag.photo_id AND photo_tag.is_deleted = false
+    AND photo_tag.tag_id = tag.id AND tag.is_deleted = false
+    `,
+    [photoId, userId],
+  );
+  const data = {
+    id: rows[0].id,
+    imageUrl: rows[0].image_url,
+    tags: tag,
+  };
+  return convertSnakeToCamel.keysToCamel(data);
+};
 export default {
   createPhotoTag,
   getTagByPhotoId,
+  getPhotoById,
 };
