e3bbec789273923048ce40561c6571804365574c
kimminwook <coreminw@naver.com>
2022-07-18 04:34:42 +0900

    [FEAT] 푸시 알림 삭제기능, 사진 존재 여부 기능 추가

--- src/services/PhotoService.ts
+++ src/services/PhotoService.ts
@@ -13,23 +13,44 @@ const test = async (client: any) => {
 };
 
 const deletePhoto = async (client: any, photoId: number, userId: number) => {
-  console.log(photoId);
+  //사진이 존재하는지 여부 확인
+  const { rows: checkPhotoExist } = await client.query(
+    `SELECT count(*)
+    FROM photo
+    WHERE id in (${photoId})
+    AND user_id = $1
+    AND is_deleted = false;
+    `,
+    [userId]
+  );
+  
+  if ((checkPhotoExist[0].count as number) <= 0) {
+    throw 404;
+  }
+
   const { rows: photo } = await client.query(
     `UPDATE photo 
     SET is_deleted = true 
-    WHERE id in (${photoId});`
+    WHERE id in (${photoId})
+    AND user_id = $1;
+    `,
+    [userId]
   );
 
   const { rows } = await client.query(
     `UPDATE photo_tag
     SET is_deleted = true 
-    WHERE photo_id in (${photoId})
-    RETURNING *;`
+    WHERE photo_id in (${photoId});
+    `,
   );
+  // 푸시 알림 삭제
   const { rows: push } = await client.query(
     `UPDATE push
     SET is_deleted = false
-    WHERE photo_id in (${photoId});`
+    WHERE photo_id in (${photoId})
+    AND user_id = $1;
+    `,
+    [userId]
   );
 
   for (let i of rows) {
@@ -37,8 +58,9 @@ const deletePhoto = async (client: any, photoId: number, userId: number) => {
       `SELECT *
     FROM photo_tag
     WHERE tag_id = $1
-    AND is_deleted = false;`,
-      [i.tag_id]
+    AND is_deleted = false
+    AND user_id = $2;`,
+      [i.tag_id, userId]
     );
     if (photoTag.length == 0) {
       const { rows: tag } = await client.query(
