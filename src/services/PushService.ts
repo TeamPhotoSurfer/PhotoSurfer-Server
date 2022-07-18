bab60907dfe4fddfd3bfb01d1a5ab67bda712b88
kimminwook <coreminw@naver.com>
2022-07-18 18:24:58 +0900

    [CHORE] 변수명 수정 및 PushService 수정

--- src/services/PushService.ts
+++ src/services/PushService.ts
@@ -1,21 +1,19 @@
 import { convertDateForm } from "../modules/convertDateForm";
-import dayjs from "dayjs";
-import { KeyObject } from "crypto";
 
 const convertSnakeToCamel3 = require("../modules/convertSnakeToCamel");
 
 const getTodayPush = async (client: any, userId: number) => {
-  const date = new Date();
-  date.setDate(date.getDate());
-  date.setHours(0, 0, 0, 0);
+  const today = new Date();
+  today.setDate(today.getDate());
+  today.setHours(0, 0, 0, 0);
 
-  const date2 = new Date();
-  date2.setDate(date2.getDate() + 1);
-  date2.setHours(0, 0, 0, 0);
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
 
-  const date3 = new Date();
-  date3.setDate(date3.getDate() + 2);
-  date3.setHours(0, 0, 0, 0);
+  const dayAfterTomorrow = new Date();
+  dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
+  dayAfterTomorrow.setHours(0, 0, 0, 0);
 
   //다가오는 알림 "오늘"
   const { rows } = await client.query(
@@ -25,7 +23,7 @@ const getTodayPush = async (client: any, userId: number) => {
     AND $2 <= push.push_date AND push.push_date < $3 AND push.photo_id = photo.id 
     ORDER BY push.push_date ASC
     `,
-    [userId, date, date2]
+    [userId, today, tomorrow]
   );
   for (let i of rows) {
     const photoId = i.photo_id;
@@ -42,17 +40,17 @@ const getTodayPush = async (client: any, userId: number) => {
     i.tags = result;
   }
   //다가오는 알림 "내일"
-  const { rows: tomorrow } = await client.query(
+  const { rows: pushTomorrow } = await client.query(
     `SELECT push.id, push.push_date, photo.image_url, push.memo, push.photo_id
     FROM push, photo
     WHERE push.user_id = $1
     AND $2 <= push.push_date AND push.push_date < $3 AND push.photo_id = photo.id 
     ORDER BY push.push_date ASC
     `,
-    [userId, date2, date3]
+    [userId, tomorrow, dayAfterTomorrow]
   );
 
-  for (let i of tomorrow) {
+  for (let i of pushTomorrow) {
     const photoId = i.photo_id;
     const { rows: tags } = await client.query(
       `SELECT tag.name
@@ -79,18 +77,6 @@ const getTodayPush = async (client: any, userId: number) => {
     `,
     [userId, dateCount]
   );
-  for (let i of last) {
-    const photoId = i.photo_id;
-    const { rows: tags } = await client.query(
-      `SELECT tag.name
-        FROM photo_tag, tag
-        WHERE photo_tag.photo_id = $1
-        AND photo_tag.is_represent = true
-        AND photo_tag.tag_id = tag.id
-        `,
-      [photoId]
-    );
-  }
 
   //다가오는 알림 개수
   const dateCome = new Date();
@@ -107,26 +93,14 @@ const getTodayPush = async (client: any, userId: number) => {
     `,
     [userId, dateCome, dateCome2]
   );
-  for (let i of come) {
-    const photoId = i.photo_id;
-    const { rows: tags } = await client.query(
-      `SELECT tag.name
-        FROM photo_tag, tag
-        WHERE photo_tag.photo_id = $1
-        AND photo_tag.is_represent = true
-        AND photo_tag.tag_id = tag.id
-        `,
-      [photoId]
-    );
-  }
 
-  let todayTomorrowCount = rows.length + tomorrow.length;
+  let todayTomorrowCount = rows.length + pushTomorrow.length;
   rows.map((x) => (x.push_date = convertDateForm(x.push_date)));
-  tomorrow.map((x) => (x.push_date = convertDateForm(x.push_date)));
+  pushTomorrow.map((x) => (x.push_date = convertDateForm(x.push_date)));
 
   const data = {
     today: [rows],
-    tomorrow: [tomorrow],
+    tomorrow: [pushTomorrow],
     todayTomorrowCount,
     lastCount: +last[0].count,
     comingCount: +come[0].count,
