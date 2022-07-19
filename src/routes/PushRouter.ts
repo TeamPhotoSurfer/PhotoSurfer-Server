5b627dc660f5e3688613cb8050a3da067b7076c6
namjiyun <namjiyun3275@gmail.com>
2022-07-19 20:55:50 +0900

    [FIX] 라우팅 위치 변경

--- src/routes/PushRouter.ts
+++ src/routes/PushRouter.ts
@@ -4,11 +4,10 @@ import PushController from '../controllers/PushController';
 
 const router: Router = Router();
 
-router.post('/:photoId', PushController.createPush);
-router.get('/:pushId', PushController.getPushDetail);
 router.get('/test/p', PushController.pushPlan);
 router.get('/come', PushController.getComePush);
 router.get('/today', PushController.getTodayPush);
 router.get('/last', PushController.getLastPush);
-
+router.post('/:photoId', PushController.createPush);
+router.get('/:pushId', PushController.getPushDetail);
 export default router;
