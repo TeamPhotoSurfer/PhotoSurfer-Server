8e0654f836a739eeff52200c34a530acfcdabf60
namjiyun <namjiyun3275@gmail.com>
2022-07-20 01:30:04 +0900

    [FIX] 라우팅 수정

--- src/routes/PhotoRouter.ts
+++ src/routes/PhotoRouter.ts
@@ -7,7 +7,7 @@ const router: Router = Router();
 
 router.post('/', upload.single('file'), PhotoController.createPhotoTag);
 router.get('/search', PhotoController.findPhotoByTag);
-router.get('/search/:photoId', PhotoController.getPhoto);
+router.get('/get/:photoId', PhotoController.getPhoto);
 router.post('/menu/tag', PhotoController.addPhotoTag);
 
 router.put('/menu/tag/:tagId', PhotoController.updatePhotoTag);
