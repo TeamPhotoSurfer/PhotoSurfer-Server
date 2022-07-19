783a753c0b2039ce358e16aa73a7a22b5959a4d6
namjiyun <91375028+junjuning@users.noreply.github.com>
2022-07-20 00:55:45 +0900

    Update PhotoRouter.ts

--- src/routes/PhotoRouter.ts
+++ src/routes/PhotoRouter.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 import upload from '../config/multer';
 import PhotoController from '../controllers/PhotoController';
-// import auth from '../modules/auth';
 
 const router: Router = Router();
 
