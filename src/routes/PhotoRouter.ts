b81b17436e642a063dcd3139714f567419b38c98
namjiyun <namjiyun3275@gmail.com>
2022-07-20 01:20:36 +0900

    [FIX] 라우팅 수정

--- src/routes/PhotoRouter.ts
+++ src/routes/PhotoRouter.ts
@@ -6,9 +6,9 @@ import PhotoController from '../controllers/PhotoController';
 const router: Router = Router();
 
 router.post('/', upload.single('file'), PhotoController.createPhotoTag);
+router.get('/', PhotoController.findPhotoByTag);
 
 router.post('/tag', PhotoController.addPhotoTag);
-router.get('/search', PhotoController.findPhotoByTag);
 router.get('/:photoId', PhotoController.getPhoto);
 router.put('/tag/:tagId', PhotoController.updatePhotoTag);
 router.delete('/tag/:tagId', PhotoController.deletePhotoTag);
--- src/routes/PushRouter.ts
+++ src/routes/PushRouter.ts
@@ -5,9 +5,9 @@ import PushController from '../controllers/PushController';
 const router: Router = Router();
 
 router.get('/test/p', PushController.pushPlan);
-router.get('/come', PushController.getComePush);
-router.get('/today', PushController.getTodayPush);
-router.get('/last', PushController.getLastPush);
+router.get('/list/come', PushController.getComePush);
+router.get('/list/today', PushController.getTodayPush);
+router.get('/list/last', PushController.getLastPush);
 router.post('/:photoId', PushController.createPush);
 router.get('/:pushId', PushController.getPushDetail);
 export default router;
