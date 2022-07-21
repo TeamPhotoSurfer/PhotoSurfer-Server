f9dd9367c3211b67ac6ab216ea88a86612c27608
kimminwook <coreminw@naver.com>
2022-07-21 18:14:06 +0900

    [CHORE] PushService 수정

--- src/services/PushService.ts
+++ src/services/PushService.ts
@@ -262,7 +262,7 @@ const getLastPush = async (client: any, userId: number) => {
   for (let i of rows) {
     const photoId = i.photo_id;
     const { rows: tags } = await client.query(
-      `SELECT tag.name
+      `SELECT tag.id ,tag.name
           FROM photo_tag, tag
           WHERE photo_tag.photo_id = $1
           AND photo_tag.is_represent = true
@@ -270,8 +270,10 @@ const getLastPush = async (client: any, userId: number) => {
           `,
       [photoId]
     );
-    const result = tags.map((x) => x.name);
-    i.tags = result;
+    // const result = tags.map((x) => x.name);
+    // i.tags = result;
+    i.tags = [];
+    i.tags = tags;
   }
 
   let totalCount = rows.length;
@@ -309,7 +311,7 @@ const getComePush = async (client: any, userId: number) => {
   for (let i of rows) {
     const photoId = i.photo_id;
     const { rows: tags } = await client.query(
-      `SELECT tag.name
+      `SELECT tag.id ,tag.name
         FROM photo_tag, tag
         WHERE photo_tag.photo_id = $1
         AND photo_tag.is_represent = true
@@ -317,8 +319,11 @@ const getComePush = async (client: any, userId: number) => {
         `,
       [photoId]
     );
-    const result = tags.map((x) => x.name);
-    i.tags = result;
+    i.tags = [];
+    i.tags = tags;
+    
+    // const result = tags.map((x) => x.name);
+    // i.tags = result;
   }
 
   let totalCount = rows.length;
@@ -348,7 +353,6 @@ const getTodayPush = async (client: any, userId: number) => {
 
   let todayPush = [];
   let tomorrowPush = [];
-  let aa = {};
   //다가오는 알림 "오늘"
   const { rows } = await client.query(
     `SELECT push.id, push.push_date, photo.image_url, push.memo, push.photo_id
@@ -371,9 +375,6 @@ const getTodayPush = async (client: any, userId: number) => {
         `,
       [photoId]
     );
-
-    console.log(tags);
-    // const result = tags.map((x) => x.name);
     i.tags = [];
     i.tags = tags;
     const data = {
@@ -404,8 +405,6 @@ const getTodayPush = async (client: any, userId: number) => {
         `,
       [photoId]
     );
-    console.log(tags);
-    // const result = tags.map((x) => x.name);
     i.tags = [];
     i.tags = tags;
     const data = {
@@ -441,7 +440,7 @@ const getTodayPush = async (client: any, userId: number) => {
   dateCome2.setHours(0, 0, 0, 0);
 
   const { rows: come } = await client.query(
-    `SELECT *
+    `SELECT count(*)
     FROM push, photo
     WHERE push.user_id = $1
     AND $2 <= push.push_date AND push.push_date <= $3 AND push.photo_id = photo.id
